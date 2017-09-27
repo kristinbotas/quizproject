e909097a2b3cbd577ea7e8d54254e4c3a4846d5b
kristinbotas <32261361+kristinbotas@users.noreply.github.com>
2017-09-27 11:27:12 +0200

    connected url:s and views

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+def startpage(request):
+		return render(request, "startpage.html")
+
+def startpage(request):
+		return render(request, "startpage.html")
+
+def quiz(request, quiz_number):
+		return render(request, "quiz.html")
+
+def question(request, quiz_number, question_number):
+		return render(request, "question.html")		
+
+def completed(request, quiz_number):
+		return render(request, "completed.html")					
+
 # Create your views here.
--- quizsite/settings.py
+++ quizsite/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+        OS.PATH.JOIN(BASE_DIR, "STATIC"),
+)
