8cfe5fd2e39d5aa98e846ac3d227c064f1e97e1c
kristinbotas <32261361+kristinbotas@users.noreply.github.com>
2017-09-27 11:45:43 +0200

    added test data

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
-def startpage(request):
-		return render(request, "startpage.html")
+quizzes = [
+	{
+			"quiz_number": 1,
+			"name": "Klassiska böcker",
+			"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+			"quiz_number": 2, 
+			"name": "Största fotbollslagen",
+			"description": "Kan du dina lag?"
+	}
+	{
+			"quiz_number": 3,
+			"name": "Världens mest kända hackare",
+			"description": "Kan du din hackerhistoria?"
+	},
+]
 
 def startpage(request):
 		return render(request, "startpage.html")
