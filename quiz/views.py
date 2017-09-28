c31bdf2e276f40f0334dfa66ade2f51ffc2d8612
kristinbotas <32261361+kristinbotas@users.noreply.github.com>
2017-09-28 14:45:56 +0200

    added form

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from quiz.models import Quiz 
+from quiz.models import Quiz
+from django.shortcuts import redirect 
 
 def startpage(request):
 		context = {
@@ -15,12 +16,15 @@ def quiz(request, quiz_number):
 		return render(request, "quiz.html", context)
 
 def question(request, quiz_number, question_number):
+		quiz = Quiz.objects.get(quiz_number=quiz_number)
+		questions = quiz.questions.all()
+		question = questions[question_number - 1]
 		context = {
 				"question_number": question_number,
-				"question": "Hur många bultar har Ölandsbron?",
-				"answer1": "12",
-				"answer2": "66 400",
-				"answer3": "7 428 954",
+				"question": question.question,
+				"answer1": question.answer1,
+				"answer2": question.answer2,
+				"quiz": quiz,
 				"quiz_number": quiz_number,
 
 		}
@@ -32,6 +36,13 @@ def completed(request, quiz_number):
 				"total": 20,
 				"quiz_number": quiz_number,
 		}
-		return render(request, "completed.html", context)					
+		return render(request, "completed.html", context)
+
+def answer(request, quiz_number, question_number):
+		answer = request.POST["answer"]
+		saved_answers = request.session.get(str(quiz_number), {})
+		saved_answers[question_number] = int(answer)
+		request.session[quiz_number] = saved_answers
+		return redirect("question_page", quiz_number, question_number + 1)	
 
 # Create your views here.
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 		path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 		path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 		path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
-		path('admin/', admin.site.urls),
+		path('admin/', admin.site.urls)
+		path("quiz/<int:quiz_number>/question/<int:question_number>/answer/", views.answer, name="answer_page"),
 		
 ]
