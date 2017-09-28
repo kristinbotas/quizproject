be350154f842be60370045ce61c914a4e46af358
kristinbotas <32261361+kristinbotas@users.noreply.github.com>
2017-09-28 10:56:27 +0200

    added django superuser

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from quiz import views
+from django.contrib import admin
 
 urlpatterns = [
 		path("", views.startpage, name="start_page"),
 		path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 		path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 		path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
+		path('admin/', admin.site.urls),
+		
 ]
